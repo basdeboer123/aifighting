7a914095f2dbc0aa373e422257972be43a7e2029
serusan <57762231+serusan@users.noreply.github.com>
2021-01-11 20:43:02 +0100

    Update posescript.js

--- posescript.js
+++ posescript.js
@@ -47,7 +47,7 @@ function onResults(results) {
   if ((center_refresh % 5) == 0){
 		centery = center_update/5;
 		center_update = 0;
-	};
+	}
   if ((cdtime == 0) && (results.poseLandmarks[19]['y'] || results.poseLandmarks[20]['y'])){
 	  if ((results.poseLandmarks[19]['y'] < results.poseLandmarks[11]['y'])||(results.poseLandmarks[20]['y'] < results.poseLandmarks[10]['y'])){
 		  console.log("PEW PEW");
