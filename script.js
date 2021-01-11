294bc13574c49ff41d79d9c1ef64f6ec78114308
Bas de Boer <bas@mauritsstraatutrecht.nl>
2021-01-11 14:18:07 +0100

    smoothen speed

--- script.js
+++ script.js
@@ -25,17 +25,44 @@ var checkDead = setInterval(function() {
         block.style.animation = "block 4s infinite linear";
     }else{
         counter++;
-        if(counter/100 > 5 && counter/100 < 11){
+        if(counter/100 > 2 && counter/100 < 5){
+            block.style.animation = "block 3.9s infinite linear";
+        }
+        else if(counter/100 > 4 && counter/100 < 7){
+            block.style.animation = "block 3.8s infinite linear";
+        }
+        else if(counter/100 > 6 && counter/100 < 9){
+            block.style.animation = "block 3.7s infinite linear";
+        }
+        else if(counter/100 > 8 && counter/100 < 11){
+            block.style.animation = "block 3.6s infinite linear";
+        }
+        else if(counter/100 > 10 && counter/100 < 13){
             block.style.animation = "block 3.5s infinite linear";
         }
-        else if(counter/100 > 10 && counter/100 < 16){
+        else if(counter/100 > 12 && counter/100 < 15){
+            block.style.animation = "block 3.4s infinite linear";
+        }
+        else if(counter/100 > 14 && counter/100 < 17){
+            block.style.animation = "block 3.3s infinite linear";
+        }
+        else if(counter/100 > 16 && counter/100 < 19){
+            block.style.animation = "block 3.2s infinite linear";
+        }
+        else if(counter/100 > 18 && counter/100 < 21){
+            block.style.animation = "block 3.1s infinite linear";
+        }
+        else if(counter/100 > 20 && counter/100 < 23){
             block.style.animation = "block 3s infinite linear";
         }
-        else if(counter/100 > 15 && counter/100 < 21){
-            block.style.animation = "block 2.5s infinite linear";
+        else if(counter/100 > 22 && counter/100 < 25){
+            block.style.animation = "block 2.9s infinite linear";
         }
-        else if(counter/100 > 20){
-            block.style.animation = "block 1s infinite linear";
+        else if(counter/100 > 24 && counter/100 < 27){
+            block.style.animation = "block 2.8s infinite linear";
+        }
+        else if(counter/100 > 26 && counter/100 < 29){
+            block.style.animation = "block 2.7s infinite linear";
         }
         document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
     }
