89c857da5b9f5703fa89b5cbd3160f54d0717b14
Murtaza Latif <57993539+iMurtaza@users.noreply.github.com>
2021-01-25 16:07:12 +0100

    Update posescript2.js

--- posescript2.js
+++ posescript2.js
@@ -10,56 +10,57 @@ var cdtime2 = 0;
 var center_update = 0;
 var center_refresh = 0;
 var audioshoot = new Audio('KEK.wav');
+
 function onResults(results) {
   comm = 0;
-  center_refresh ++;
+  center_refresh++;
   canvasCtx.save();
   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
   canvasCtx.drawImage(
-      results.image, 0, 0, canvasElement.width, canvasElement.height);
-  drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
-                 {color: '#00FF00', lineWidth: 4});
-  drawLandmarks(canvasCtx, results.poseLandmarks,
-                {color: '#FF0000', lineWidth: 2});
+    results.image, 0, 0, canvasElement.width, canvasElement.height);
+  drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {
+    color: '#00FF00',
+    lineWidth: 4
+  });
+  drawLandmarks(canvasCtx, results.poseLandmarks, {
+    color: '#FF0000',
+    lineWidth: 2
+  });
 
   const kekw = results.poseLandmarks[10]['y'];
   center_update = center_update + kekw
   if (kekw != 0 && cdtime <= 0 && comm == 0) {
-	if ((kekw - centery) < -0.03){
-			console.log("Spring");
-			cdtime = 30;
-			jump()
-		}
-	else if (results.poseLandmarks[19]['y'] < results.poseLandmarks[11]['y']){
-			console.log("Buk");
-			cdtime = 30;
-			comm = -1;
-			jumpdown()
-		}
-	}
-  else if (prevkekw == 0){
-		centery = kekw;
+    if ((kekw - centery) < -0.03) {
+      console.log("Spring");
+      cdtime = 30;
+      jump()
+    } else if (results.poseLandmarks[19]['y'] < results.poseLandmarks[11]['y']) {
+      console.log("Buk");
+      cdtime = 30;
+      comm = -1;
+      jumpdown()
+    }
+  } else if (prevkekw == 0) {
+    centery = kekw;
 
 
-  }
-  else{
-		cdtime--;
-	    comm = 0;
+  } else {
+    cdtime--;
+    comm = 0;
   };
-  if ((center_refresh % 5) == 0){
-		centery = center_update/5;
-		center_update = 0;
-	};
-  if ((cdtime2 <= 0) && (results.poseLandmarks[19]['y']|| results.poseLandmarks[20]['y'])){
-	  if((results.poseLandmarks[20]['y'] < results.poseLandmarks[12]['y'])){
-		  cdtime2 = 10;
-		  console.log("Shoot");
-		  audioshoot.play();
-		  shoot();
-	  };
-  }
-  else {
-	  cdtime2--;
+  if ((center_refresh % 5) == 0) {
+    centery = center_update / 5;
+    center_update = 0;
+  };
+  if ((cdtime2 <= 0) && (results.poseLandmarks[19]['y'] || results.poseLandmarks[20]['y'])) {
+    if ((results.poseLandmarks[20]['y'] < results.poseLandmarks[12]['y'])) {
+      cdtime2 = 10;
+      console.log("Shoot");
+      audioshoot.play();
+      shoot();
+    };
+  } else {
+    cdtime2--;
   }
   prevkekw = kekw;
   canvasCtx.restore();
@@ -68,9 +69,11 @@ function onResults(results) {
 };
 
 
-const pose = new Pose({locateFile: (file) => {
-  return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
-}});
+const pose = new Pose({
+  locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
+  }
+});
 pose.setOptions({
   upperBodyOnly: true,
   smoothLandmarks: true,
@@ -80,10 +83,11 @@ pose.setOptions({
 pose.onResults(onResults);
 const camera = new Camera(videoElement, {
   onFrame: async () => {
-    await pose.send({image: videoElement});
+    await pose.send({
+      image: videoElement
+    });
   },
   width: 720,
   height: 480
 });
-
 camera.start();
